d616f5ec1ea2b1ab832ae8493e75530f74b594f4
Elena <k-i-s-u-n-j-a@list.ru>
2018-05-23 16:36:53 +0500

    Установка зависимостей в gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
+var server = require("browser-sync").create();
+var run = require("run-sequence");
