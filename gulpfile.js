58dbbb3b77d280d04ac771d575d59f89670b9f89
Elena <k-i-s-u-n-j-a@list.ru>
2018-07-18 08:55:30 +0500

    Обновлены файлы package.json, gulpfile, переустановка npm

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,7 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var minify = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
-var htmlmin = require("gulp-htmlmin");
-var uglify = require("gulp-uglify");
 var server = require("browser-sync").create();
-var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
@@ -26,13 +15,10 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", function() {
+gulp.task("serve", ["style"], function() {
   server.init({
     server: "source/",
     notify: false,
@@ -42,5 +28,5 @@ gulp.task("serve", function() {
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html", ["html"]);
+  gulp.watch("source/*.html").on("change", server.reload);
 });
