1e5fa4114393b1722faeb45502163d3e359249f2
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-06-04 14:50:20 +0600

    Сформирован массив с данными животных

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,2 +1,13 @@
-'use strict';
+`use strict`;
 
+const detailsBtns = document.querySelectorAll(`.card__button`);
+
+// Открытие окна описания животного
+const openDetailsWindow = () => {
+
+};
+
+// Закрытие окна описания животного
+const closeDetailsWindow = () => {
+
+};
